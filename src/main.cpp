0b66e770f1a5601b0a82efa88baca769c9da8f5e
Mike Killewald <mkillewald@yahoo.com>
2021-11-29 13:25:58 -0500

    changed window size to 1024x768

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 int main(int argc, char *argv[]) {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
-  constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{640};
-  constexpr std::size_t kGridHeight{640};
+  constexpr std::size_t kScreenWidth{1024};
+  constexpr std::size_t kScreenHeight{768};
+  constexpr std::size_t kGridWidth{1024};
+  constexpr std::size_t kGridHeight{768};
 
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
