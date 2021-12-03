64f9e50309f9ef5a5927a5f0ed02cc8ee3a84344
Mike Killewald <mkillewald@yahoo.com>
2021-12-03 02:19:30 -0500

    added scale

--- src/UFO.cpp
+++ src/UFO.cpp
@@ -1,5 +1,6 @@
 #include "UFO.h"
 
-UFO::UFO(const std::size_t grid_width, const std::size_t grid_height)
-    : GameObject(grid_width, grid_height) {}
+UFO::UFO(const std::size_t grid_width, const std::size_t grid_height,
+         float game_scale)
+    : GameObject(grid_width, grid_height, game_scale) {}
 UFO::~UFO() {}
--- src/UFO.h
+++ src/UFO.h
@@ -7,7 +7,8 @@ class UFO : public GameObject {
 public:
   enum UFOSize { large, small };
   // constructor / destructor
-  UFO(const std::size_t grid_width, const std::size_t grid_height);
+  UFO(const std::size_t grid_width, const std::size_t grid_height,
+      float game_scale);
   ~UFO();
 
   // getters / setters
