43c6887d4999d719c7cecd498384447cda2fe3d2
Mike Killewald <mkillewald@yahoo.com>
2021-12-16 01:03:27 -0500

    added player lives to hud

--- src/HUD.cpp
+++ src/HUD.cpp
@@ -7,62 +7,74 @@
 #include <SDL.h>
 
 #include <iostream>
+#include <string>
 
-HUD::HUD(Game const *game, Renderer const *renderer)
+HUD::HUD(Game *const game, Renderer *const renderer)
     : game_(game), player1_(game->player1()), player2_(game->player2()),
       renderer_(renderer) {
 
   centerX_ = renderer_->gridWidth() / 2;
   centerY_ = renderer_->gridHeight() / 2;
 
-  SDL_Color textColor = FC_MakeColor(255, 255, 255, 150);
+  hudColor = FC_MakeColor(255, 255, 255, 150);
 
   // Load fonts
   small_ = FC_CreateFont();
   FC_LoadFont(small_, renderer_->sdlRenderer(), "../fonts/atari_vector.ttf", 14,
-              textColor, TTF_STYLE_NORMAL);
+              hudColor, TTF_STYLE_NORMAL);
   if (nullptr == small_) {
     std::cerr << "True type font was not found." << std::endl;
     std::cerr << "SDL_Error: " << SDL_GetError() << std::endl;
   }
   medium_ = FC_CreateFont();
   FC_LoadFont(medium_, renderer_->sdlRenderer(), "../fonts/atari_vector.ttf",
-              28, textColor, TTF_STYLE_NORMAL);
+              28, hudColor, TTF_STYLE_NORMAL);
   if (nullptr == medium_) {
     std::cerr << "True type font was not found." << std::endl;
     std::cerr << "SDL_Error: " << SDL_GetError() << std::endl;
   }
   large_ = FC_CreateFont();
   FC_LoadFont(large_, renderer_->sdlRenderer(), "../fonts/atari_vector.ttf", 56,
-              textColor, TTF_STYLE_NORMAL);
+              hudColor, TTF_STYLE_NORMAL);
   if (nullptr == large_) {
     std::cerr << "True type font was not found." << std::endl;
     std::cerr << "SDL_Error: " << SDL_GetError() << std::endl;
   }
 }
 
-HUD::~HUD() { FC_FreeFont(medium_); }
+HUD::~HUD() {
+  FC_FreeFont(small_);
+  FC_FreeFont(medium_);
+  FC_FreeFont(large_);
+}
 
 void HUD::update() {
   switch (game_->state()) {
-  case Game::eGameState::kAttract_:
+  case Game::kAttract_:
     break;
-  case Game::eGameState::kReadyToPlay_:
-    if (SDL_GetTicks() - blinkTicks_ >= blinkTickLimit_) {
+  case Game::kReadyToPlay_:
+    if (SDL_GetTicks() - blinkTicks_ >= kSlowBlinkTickLimit_) {
       blink_ = !blink_;
       blinkTicks_ = SDL_GetTicks();
     }
     break;
-  case Game::eGameState::kPlay_:
+  case Game::kReadyPlayer1_:
+  case Game::kReadyPlayer2_:
+    if (SDL_GetTicks() - blinkTicks_ >= kFastBlinkTickLimit_) {
+      blink_ = !blink_;
+      blinkTicks_ = SDL_GetTicks();
+    }
     break;
-  case Game::eGameState::kHighScoreEntry_:
+  case Game::kPlay_:
+    break;
+  case Game::kHighScoreEntry_:
     break;
   }
 }
 
 void HUD::draw() const {
   switch (game_->state()) {
-  case Game::eGameState::kAttract_: {
+  case Game::kAttract_: {
     // alternate between attract and high score table
     drawP1Score();
     drawHiScore();
@@ -71,7 +83,7 @@ void HUD::draw() const {
     draw1Coin1Start();
     break;
   }
-  case Game::eGameState::kReadyToPlay_: {
+  case Game::kReadyToPlay_: {
     // alternate between attract and high score table, flash push start
     drawP1Score();
     drawHiScore();
@@ -81,34 +93,88 @@ void HUD::draw() const {
     draw1Coin1Start();
     break;
   }
-  case Game::eGameState::kPlay_:
+  case Game::kReadyPlayer1_:
+    if (game_->numPlayers() == 2) {
+      if (blink_) {
+        drawP1Score();
+      }
+      drawP2Score();
+      drawP2Lives();
+    } else {
+      drawP1Score();
+    }
+    drawP1Lives();
+    drawHiScore();
+    drawMessageCenterX(150, kPlayer1.c_str());
+
+    // TODO: if not start of game, show asteroid field
+    break;
+  case Game::kReadyPlayer2_:
+    drawP1Score();
+    drawP1Lives();
+    drawHiScore();
+    if (blink_) {
+      drawP2Score();
+    }
+    drawP2Lives();
+    drawMessageCenterX(150, kPlayer2.c_str());
+
+    // TODO: if not start of game, show asteroid field
+    break;
+  case Game::kPlay_:
     // run game
     drawP1Score();
-    // drawP1Lives();
+    drawP1Lives();
     drawHiScore();
     if (game_->numPlayers() == 2) {
       drawP2Score();
-      // drawP2Lives();
+      drawP2Lives();
     }
     break;
-  case Game::eGameState::kHighScoreEntry_:
+  case Game::kGameOver_:
+    drawMessageCenterX(250, kGameOver.c_str());
+    break;
+  case Game::kHighScoreEntry_:
     // get players initials
     drawP1Score();
     drawHiScore();
     drawP2Score();
 
+    drawScoreEntryInstructions();
     // TODO: finish this
     break;
   }
 }
 
+// TODO: fix positioning of score and lives
+
 void HUD::drawP1Score() const {
-  FC_Draw(medium_, renderer_->sdlRenderer(), 10, 3, "12345");
+  std::string score;
+  if (player1_->score() == 0) {
+    score = "00";
+  } else {
+    score = std::to_string(player1_->score());
+  }
+  FC_Draw(medium_, renderer_->sdlRenderer(), 10, 3, score.c_str());
 }
 
 void HUD::drawP2Score() const {
+  std::string score;
+  if (player2_->score() == 0) {
+    score = "00";
+  } else {
+    score = std::to_string(player2_->score());
+  }
   FC_DrawAlign(medium_, renderer_->sdlRenderer(), renderer_->gridWidth() - 10,
-               3, FC_ALIGN_RIGHT, "12345");
+               3, FC_ALIGN_RIGHT, score.c_str());
+}
+
+void HUD::drawP1Lives() const {
+  player1_->drawLives(renderer_, 100, 50, hudColor);
+}
+
+void HUD::drawP2Lives() const {
+  player2_->drawLives(renderer_, 850, 50, hudColor);
 }
 
 void HUD::drawHiScore() const {
@@ -123,7 +189,13 @@ void HUD::drawPushStart() const {
   }
 }
 
+void HUD::drawMessageCenterX(float y, const char *message) const {
+  FC_DrawAlign(medium_, renderer_->sdlRenderer(), centerX_, y, FC_ALIGN_CENTER,
+               message);
+}
+
 void HUD::drawHiScoreTable() const {
+  // TODO: finish this, dont create int inside loop, get real data.
   int tableX = centerX_ - 70;
   int tableY = 205;
   int maxWidth = 110;
@@ -143,4 +215,8 @@ void HUD::drawHiScoreTable() const {
 void HUD::draw1Coin1Start() const {
   FC_DrawAlign(medium_, renderer_->sdlRenderer(), centerX_, 640,
                FC_ALIGN_CENTER, k1Coin1Start.c_str());
+}
+
+void HUD::drawScoreEntryInstructions() const {
+  FC_Draw(medium_, renderer_->sdlRenderer(), 100, 200, kHiScoreEntry.c_str());
 }
--- src/HUD.h
+++ src/HUD.h
@@ -3,6 +3,8 @@
 
 #include "../lib/SDL_FontCache.h"
 
+#include <SDL.h>
+
 #include <string>
 
 // forward declaration to avoid include cycle
@@ -12,7 +14,7 @@ class PlayerController;
 
 class HUD {
 public:
-  HUD(Game const *game, Renderer const *renderer);
+  HUD(Game *const game, Renderer *const renderer);
   ~HUD();
 
   // getters / setters
@@ -22,29 +24,44 @@ public:
   void update();
 
 private:
+  const Uint32 kFastBlinkTickLimit_{250};
+  const Uint32 kSlowBlinkTickLimit_{500};
   const std::string kHighScores{"HIGH SCORES"};
   const std::string kPushStart{"PUSH START"};
   const std::string k1Coin1Start{"1 COIN 1 START"};
+  const std::string kPlayer1{"PLAYER 1"};
+  const std::string kPlayer2{"PLAYER 2"};
+  const std::string kGameOver{"GAME OVER"};
+  const std::string kHiScoreEntry{"YOUR SCORE IS ONE OF THE TEN BEST\n"
+                                  "PLEASE ENTER YOUR INITIALS\n"
+                                  "PUSH ROTATE TO SELECT LETTER\n"
+                                  "PUSH HYPERSPACE WHEN LETTER IS CORRECT"};
 
-  Game const *game_ = nullptr;
-  Renderer const *renderer_ = nullptr;
-  PlayerController const *player1_ = nullptr;
-  PlayerController const *player2_ = nullptr;
+  Game *const game_ = nullptr;
+  Renderer *const renderer_ = nullptr;
+  PlayerController *const player1_ = nullptr;
+  PlayerController *const player2_ = nullptr;
+  SDL_Color hudColor;
   FC_Font *small_ = nullptr;
   FC_Font *medium_ = nullptr;
   FC_Font *large_ = nullptr;
+
   int centerX_;
   int centerY_;
   Uint32 blinkTicks_;
-  Uint32 blinkTickLimit_{1000};
   bool blink_{false};
 
   void drawP1Score() const;
   void drawP2Score() const;
+  void drawP1Lives() const;
+  void drawP2Lives() const;
+
   void drawHiScore() const;
   void drawPushStart() const;
+  void drawMessageCenterX(float y, const char *message) const;
   void drawHiScoreTable() const;
   void draw1Coin1Start() const;
+  void drawScoreEntryInstructions() const;
 };
 
 #endif
